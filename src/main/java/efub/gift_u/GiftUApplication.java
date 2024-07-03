0bdb8b0fc2feb4ab0ec828d6202b3da280ec9754
이여진 <yeojinlee1020@gmail.com>
2024-07-03 17:06:28 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/efub/gift_u/GiftUApplication.java
@@ -0,0 +1,13 @@
+package efub.gift_u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftUApplication.class, args);
+	}
+
+}
