bca251dd1bc052c09c28451b0314cce7f9b39228
이여진 <yeojinlee1020@gmail.com>
2024-08-02 18:41:21 +0900

    feat: 날짜별 마감펀딩 유무 조회

--- src/main/java/efub/gift_u/domain/funding/dto/AllFundingResponseDto.java
+++ src/main/java/efub/gift_u/domain/funding/dto/AllFundingResponseDto.java
@@ -9,4 +9,8 @@ import java.util.List;
 @AllArgsConstructor
 public class AllFundingResponseDto {
     private List<IndividualFundingResponseDto> fundings;
+
+    public boolean isEmpty() {
+        return fundings.isEmpty();
+    }
 }
