24460764cc9055fe5429a98a4d40aae061c4a652
이여진 <yeojinlee1020@gmail.com>
2024-08-02 19:26:06 +0900

    fix: 유저 삭제 외래키 참조로 인한 에러 해결

--- src/main/java/efub/gift_u/domain/funding/repository/FundingRepository.java
+++ src/main/java/efub/gift_u/domain/funding/repository/FundingRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface FundingRepository extends JpaRepository<Funding, Long> {
 
