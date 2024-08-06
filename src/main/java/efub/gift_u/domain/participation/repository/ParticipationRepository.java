a6357f8806ab98c99eb750372ef30c37fcdd73ee
oohyj <hyj0660@gmail.com>
2024-08-06 22:32:58 +0900

    refactor : 불필요한 코드 제거

--- src/main/java/efub/gift_u/domain/participation/repository/ParticipationRepository.java
+++ src/main/java/efub/gift_u/domain/participation/repository/ParticipationRepository.java
@@ -34,8 +34,5 @@ public interface ParticipationRepository extends  JpaRepository<Participation, L
     @Query("select p.createdAt FROM Participation p where p.user.userId =:userId and p.funding.fundingId =:fundingId ORDER BY  p.createdAt DESC")
     Optional<LocalDateTime> findCreatedAtByUserIdAndFundingId(@Param("userId") Long userId , @Param("fundingId") Long fundingId);
 
-    @Query("select p FROM Participation p where p.user.userId =:userId and p.funding.fundingId =:fundingId ORDER BY  p.createdAt DESC")
-    List<Participation> findCreatedAtByUserIdAndFundingId1(@Param("userId") Long userId , @Param("fundingId") Long fundingId);
-
-
+    
 }
